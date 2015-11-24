bc56e9e1f968f7c7c141833b05290071858929a0
Sean Eshbaugh <seaneshbaugh@gmail.com>
2015-11-24 14:44:19 -0600

    Initial Commit

---
+++ MediaStreamerClient/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MediaStreamerClient
+//
+//  Created by Sean Eshbaugh on 11/24/15.
+//  Copyright © 2015 Sean Eshbaugh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
